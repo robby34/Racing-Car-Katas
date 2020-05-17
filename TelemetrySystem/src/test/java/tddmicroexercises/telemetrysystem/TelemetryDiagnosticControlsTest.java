b6ba618e57329b26ef5a3c2dbbb1415c8765b2ec
Olivier ROBIN <olivier.robin@neo-soft.fr>
2020-05-17 20:59:18 +0200

    Add TUs for the method checkTransmission, more complex case because of the dependency to TelemetryClient (need to use PowerMock)

--- TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,18 +1,31 @@
 package tddmicroexercises.telemetrysystem;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TelemetryDiagnosticControls.class)
 public class TelemetryDiagnosticControlsTest {
 
     private TelemetryDiagnosticControls telemetryDiagnosticControls;
 
+    @Mock
+    TelemetryClient mockedTelemetryClient;
+
     @Before
-    public void setup() {
+    public void setup() throws Exception {
+        PowerMockito.whenNew(TelemetryClient.class).withNoArguments().thenReturn(mockedTelemetryClient);
         telemetryDiagnosticControls = new TelemetryDiagnosticControls();
     }
 
@@ -40,7 +53,47 @@ public class TelemetryDiagnosticControlsTest {
     }
 
     @Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() {
+    public void testCheckTransmissionSuccess() throws Exception {
+        // Given the mocked TelemetryClient responding false and then always true on 'getOnlineStatus'
+        Mockito.when(mockedTelemetryClient.getOnlineStatus()).thenReturn(false).thenReturn(true);
+        // And responding a fake diagnostic result on 'receive'
+        Mockito.when(mockedTelemetryClient.receive()).thenReturn("fake diagnostic result");
+
+        // When checkTransmission is called
+        telemetryDiagnosticControls.checkTransmission();
+
+        // Then it should call the TelemetryClient for 1 disconnection, 1 connection, and 3 status
+        Mockito.verify(mockedTelemetryClient, Mockito.times(1)).disconnect();
+        Mockito.verify(mockedTelemetryClient, Mockito.times(1)).connect("*111#");
+        Mockito.verify(mockedTelemetryClient, Mockito.times(3)).getOnlineStatus();
+        // Then it should call it for sending a diagnostic message
+        Mockito.verify(mockedTelemetryClient, Mockito.times(1)).send("AT#UD");
+        // Then it should call it to receive the diagnostic result message
+        Mockito.verify(mockedTelemetryClient, Mockito.times(1)).receive();
+        // Then it should have the diagnostic result message
+        assertEquals("fake diagnostic result", telemetryDiagnosticControls.getDiagnosticInfo());
+    }
+
+    @Test
+    public void testCheckTransmissionConnectionFailure() throws Exception {
+        // Given the mocked TelemetryClient responding always false on 'getOnlineStatus'
+        Mockito.when(mockedTelemetryClient.getOnlineStatus()).thenReturn(false);
+
+        // When checkTransmission is called, an Exception is raised
+        try {
+            telemetryDiagnosticControls.checkTransmission();
+            Assert.fail("Should have thrown Exception");
+        } catch (Exception e) {
+            assertEquals("Unable to connect.", e.getMessage());
+        }
+
+        // Then it should call the TelemetryClient for 1 disconnection, 3 connection attempts , and 5 status
+        Mockito.verify(mockedTelemetryClient, Mockito.times(1)).disconnect();
+        Mockito.verify(mockedTelemetryClient, Mockito.times(3)).connect("*111#");
+        Mockito.verify(mockedTelemetryClient, Mockito.times(5)).getOnlineStatus();
+        // Then it should never call 'send' and 'receive' methods
+        Mockito.verify(mockedTelemetryClient, Mockito.never()).send(Mockito.anyString());
+        Mockito.verify(mockedTelemetryClient, Mockito.never()).receive();
     }
 
 }
